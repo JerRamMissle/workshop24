572c938077e293cf5d43cc7c92346897c08d5baa
JerRamMissle <114175041+JerRamMissle@users.noreply.github.com>
2023-06-28 21:38:36 -0400

    need ignore for node modules
    avoiding pushing modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { puppyList } from './data.js'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
